b2d0ddec6ca1273f9feedbc4f070e8b3b153928e
Steven Bell <dev.thebells1111@gmail.com>
2023-02-17 15:21:08 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Editor from './Editor.svelte';
+</script>
+
+<Editor />
---
+++ src/routes/Editor.svelte
@@ -0,0 +1,33 @@
+<script>
+	let bands = [];
+	let albums = [];
+	let tracks = [];
+	let selectedScreen = 'bands';
+
+	function handleScreenSelect(screen) {
+		selectedScreen = screen;
+	}
+</script>
+
+<ul>
+	<li
+		on:click={handleScreenSelect.bind(this, 'bands')}
+		on:keypress={handleScreenSelect.bind(this, 'bands')}
+	>
+		Bands
+	</li>
+	<li
+		on:click={handleScreenSelect.bind(this, 'albums')}
+		on:keypress={handleScreenSelect.bind(this, 'albums')}
+	>
+		Albums
+	</li>
+	<li
+		on:click={handleScreenSelect.bind(this, 'tracks')}
+		on:keypress={handleScreenSelect.bind(this, 'tracks')}
+	>
+		Tracks
+	</li>
+</ul>
+
+{selectedScreen}
