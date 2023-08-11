23f4824660ef9e58481bf30499809abc811372ae
Steven Bell <dev.thebells1111@gmail.com>
2023-08-11 14:28:51 -0500

    working on WP tutorial

--- src/routes/Tutorial/SetupFolders/3.svelte
+++ src/routes/Tutorial/SetupFolders/3.svelte
@@ -1,1 +1,8 @@
-3
+<p>
+	Your file names can be anything you want, these are simply generic names for this tutorial.
+	However, do keep your names a reasonable length, as the file name doesn't particularly matter for
+	the players.
+</p>
+
+<style>
+</style>
