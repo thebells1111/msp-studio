4eca4dda79fb92c253ab2077ac6c61783e23faa3
Steven Bell <dev.thebells1111@gmail.com>
2023-08-11 22:52:39 -0500

    more work on tuts

--- src/routes/Tutorial/MSP/0.svelte
+++ src/routes/Tutorial/MSP/0.svelte
@@ -1,1 +1,11 @@
-MSP Build
+<p>Keep your WordPress tab open.</p>
+
+<p>Open MSP in a new tab so you can build your feed while keeping this tutorial open this tab.</p>
+
+<p>
+	Alternatively, you can minimize this tutorial by clicking the 'x' in the upper left. To come back,
+	click the tutorial button.
+</p>
+<p>
+	However, it's easier keeping the tutorial open in one tab and building the feed in the other tab.
+</p>
---
+++ src/routes/Tutorial/MSP/7.svelte
@@ -0,0 +1,20 @@
+<p>
+	Now, the easiest way to get started with v4v is to sign up with <a href="https://getalby.com/"
+		>Alby</a
+	>. Just click the link and Create an Account.
+</p>
+
+<p>
+	Once you have your Alby account (or other lightning address, like Fountain, v4v.app, or your own
+	node), click the blue add person icon. You'll see a Blank Person show up. Click that Blank Person.
+</p>
+
+<style>
+	img {
+		margin: 0 auto;
+	}
+
+	li {
+		padding: 2px 0;
+	}
+</style>
