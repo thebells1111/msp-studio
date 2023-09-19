6e9f7032a74fa54454598f299b9ac19846a7f6af
Steven Bell <dev.thebells1111@gmail.com>
2023-09-19 14:10:14 -0500

    add og titles to blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,13 @@
+<svelte:head>
+	<meta
+		property="og:title"
+		content="Not your hosting, Not your feed - Why bitcoin podcasters and musicians should consider self-hosting"
+	/>
+	<meta property="og:type" content="blog" />
+	<meta property="og:url" content="https://musicsideproject.com/blog" />
+	<meta property="og:image" content="https://musicsideproject.com/msp-record-300.png" />
+</svelte:head>
+
 <nav><a class="back" href="/">Back</a></nav>
 
 <article>
